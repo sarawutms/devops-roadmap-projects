4124c0052e52fefa8ed4c164b8c4cda935f2e164
sarawutms <88425757+sarawutms@users.noreply.github.com>
2026-04-03 10:34:17 +0700

    refactor: extract server credentials to deploy.conf

--- static-site-server/deploy.sh
+++ static-site-server/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Your Server
-USER="ubuntu"
-IP="YOUR_SERVER_IP"
-TARGET_DIR="/var/www/html"
+# get deploy.conf
+source deploy.conf
 
-echo "🚚 Deploying files to server..."
-rsync -avz --exclude='.git' --exclude='deploy.sh' ./ $USER@$IP:$TARGET_DIR
+echo "🚚 Deploying files to $SERVER_IP..."
+rsync -avz --exclude='.git' --exclude='deploy.sh' --exclude='deploy.conf' ./ $SERVER_USER@$SERVER_IP:$TARGET_DIR
 echo "🚀 Done!"
